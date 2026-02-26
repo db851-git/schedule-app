ff3b649faa5d503351646c5f1062be3bc450921b
db851-git <db851@exeter.ac.uk>
2026-02-26 09:16:00 +0000

    Added DB connection pooling to prevent cold-start 500 errors

--- config.py
+++ config.py
@@ -8,6 +8,12 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///schedule.db'
     FREE_PLAN_TASK_LIMIT = 5
+    
+    # NEW: Database connection pooling to prevent 500 Errors after server sleep
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,  # Checks if the connection is alive before using it
+        "pool_recycle": 300,    # Recycles connections older than 5 minutes (300 seconds)
+    }
 
 class DevelopmentConfig(Config):
     DEBUG = True
